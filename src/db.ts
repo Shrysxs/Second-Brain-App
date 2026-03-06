13cc6a96645f44c0e78de884b639ae908de8de32
Shrysxs <shreyashborade2@gmail.com>
2026-03-07 01:36:33 +0530

    signup endpoint and content schema (yes ill fix it in morning)

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,23 @@
-import {model, Schema} from "mongoose";
+import mongoose, { connect, model, Schema, Types } from "mongoose";
+
+connect("mongodb://localhost:27017/brainly");
 
 
 const UserSchema = new Schema({
-    username: {type: String, unique:true}
-    ,password: {type: String, required:true}
+    username: { type: String, unique: true }
+    , password: { type: String, required: true }
 })
 
-export const UserModel = model ("User", UserSchema);
+export const UserModel = model("User", UserSchema);
 
+const ContentSchema = new Schema({
+    title: String,
+    link: String,
+    tags: [{ types: mongoose, Types, ObjectId, ref: 'Tag' }],
+    userId: {
+        types: mongoose, Types, ObjectId, ref: 'User',
+        required: true
+    }
+})
+
+export const ContentModel = model, { "content", ContentSchema };
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 import { UserModel } from "./db.js";
 
+const JWT_PASSWORD = "123123"
+
 const app = express();
 app.use(express.json());
 
@@ -10,19 +12,43 @@ app.post("/api/v1/signup", async (req, res) => {
     //TODO: ZOD VALIDATION , HASH THE PASSWORD
     const username = req.body.username;
     const password = req.body.password;
+    try {
+        await UserModel.create({
+            username: username,
+            password: password,
+        })
 
-    await UserModel.create({
-        username: username,
-        password: password,
-
-    })
-    res.json({
-        message: "User signed Up"
-    })
+        res.json({
+            message: "User signed Up"
+        })
+    } catch (e) {
+        res.status(411).json({
+            message: "User already exists"
+        });
+    }
 });
 
-app.post("/api/v1/signin", (req, res) => {
+app.post("/api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    });
 
+    if (existingUser) {
+        const token = jwt.sign({
+            id: existingUser._id
+        }, JWT_PASSWORD);
+
+        res.json({
+            token
+        });
+    } else {
+        res.status(403).json({
+            message: "Incorrect Credentials"
+        });
+    }
 });
 
 app.post("/api/v1/content", (req, res) => {
